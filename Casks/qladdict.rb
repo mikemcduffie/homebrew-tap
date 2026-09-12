3ed84ad019f62394a8d4b11b54454a42ee771170
mikemcduffie <mikemcduffie@users.noreply.github.com>
2026-09-11 20:21:24 -0400

    updated postflight_steps

--- Casks/qladdict.rb
+++ Casks/qladdict.rb
@@ -17,7 +17,7 @@ cask "qladdict" do
     run "/usr/bin/xattr", base: :appdir, sudo: true, args: [
       "-dr",
       "com.apple.quarantine",
-      "/Users/#{target_user}/Library/QuickLook/QLAddict.qlgenerator",
+      "/Users/#{user}/Library/QuickLook/QLAddict.qlgenerator",
     ]
     run "echo", args: ["Restarting quicklook service"], print_stdout: true
     run "/usr/bin/qlmanage", args: ["-r"]
