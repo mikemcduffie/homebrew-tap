70af126c87b60db2271aa6575aab4d0770fc1d9a
mikemcduffie <mikemcduffie@users.noreply.github.com>
2026-09-11 20:11:08 -0400

    updated postflight_steps

--- Casks/qladdict.rb
+++ Casks/qladdict.rb
@@ -14,7 +14,7 @@ cask "qladdict" do
 
   postflight_steps do
     run "echo", args: ["Releasing #{token} from quarantine"], print_stdout: true
-    run "/usr/bin/xattr", base: :appdir, sudo:, args: [
+    run "/usr/bin/xattr", base: :appdir, sudo: true, args: [
       "-dr",
       "com.apple.quarantine",
       "/Users/mike/Library/QuickLook/QLAddict.qlgenerator",
